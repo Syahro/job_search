488dfeb1233e7a95b4cb596a7504ade7639dd91f
Arief Syahroni <riefsyah31@gmail.com>
2021-06-30 15:31:02 +0700

    Customize theme and add new assets images

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+Color blueColor = Color(0XFF426FB4);
+Color buttonBlueColor = Color(0XFF5C9DFF);
+Color whiteColor = Color(0XFFFFFFFF);
+Color blackColor = Color(0XFF000000);
+Color darkGrey = Color(0XFF717171);
+Color lightGrey = Color(0XFFF6F6F6);
+Color yellowColor = Color(0XFFFCDA64);
+
+TextStyle titleLandingTextStyle = GoogleFonts.nunito(
+  fontSize: 24,
+  fontWeight: FontWeight.w800,
+  color: blueColor,
+);
+
+TextStyle subTitleLandingTextStyle = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  color: blueColor,
+);
